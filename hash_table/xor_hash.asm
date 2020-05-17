deea3d9c89ae7651db9282a893e984b48a9d3437
Demina Elizaveta <liza_demina11@mail.ru>
2020-05-18 02:35:51 +0300

    Improve asm hash

--- hash_table/xor_hash.asm
+++ hash_table/xor_hash.asm
@@ -2,21 +2,20 @@
 .model tiny, c
 .code
 
-
-_XORHashAsm proc string:ptr byte, len:dword
-	mov ecx, len
+_XORHashAsm proc string:ptr byte
 	mov edx, dword ptr string
-
 	xor eax, eax			; eax = 0
 	xor ebx, ebx			; ebx = 0
 	hash_loop:
 		mov bl, [edx]		; bl = str[i]
+		cmp bl, 0h
+		je exit
 		xor eax, ebx		; eax ^= ebx
 		rol eax, 1			; eax circular shift right 1
 		inc edx				; edx++
-		loop hash_loop		
-
-	ret
+		jmp hash_loop		
+	exit:
+		ret
 _XORHashAsm endp
 
 end
